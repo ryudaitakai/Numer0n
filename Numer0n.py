9c405f7ddd3d24a2599cd5c0ad5a97c9733922fa
高井 <56185180+ryudaitakai@users.noreply.github.com>
2021-04-03 15:06:57 +0900

    second commit

--- Numer0n.py
+++ Numer0n.py
@@ -1,77 +1,71 @@
 import random
 
-num1 = random.randint(0, 9)
-num2 = random.randint(0, 9)
-num3 = random.randint(0, 9)
+
+numlist = list(range(10))
+
+num1 = numlist.pop(random.randint(0, len(numlist) - 1))
+num2 = numlist.pop(random.randint(0, len(numlist) - 1))
+num3 = numlist.pop(random.randint(0, len(numlist) - 1))
 
 answer = [num1, num2, num3]
 
+
+
 def check_digit(num):
-    if num[0] == num[1] or num[1] == num[2] or num[2] == num[0]:
-        return True
-    else:
-        return False
-
-while check_digit(answer):
-    num2 = random.randint(0, 9)
-    num3 = random.randint(0, 9)
-    answer = [num1, num2, num3]
+    return len(num) != len(set(num))
 
 
-
-print("Let's start Numer0n!")
-
-
-
-def eat_judge(answer, val):
+def eat_judge(answer, num_3dig):
     eat = 0
     for n in range(len(answer)):
-        if answer[n] == val[n]:
+        if answer[n] == num_3dig[n]:
             eat += 1
     return eat
 
 
-def bite_judge(answer, val):
+def bite_judge(answer, num_3dig):
     bite = 0
     for n in range(len(answer)):
         for m in range(len(answer)):
-            if answer[n] == val[m] and n != m:
+            if answer[n] == num_3dig[m] and n != m:
                 bite += 1
     return bite
 
 
 
+print("Let's start Numer0n!")
+
 n = 1
 while n < 11:
     print("Challenge " + str(n))
 
-    val = input('Please enter a 3-digit number : ')
+    num_3dig = input('Please enter a 3-digit number : ')
 
     while True:
-        if len(val) != 3:
-            val = input('Please enter a 3-digit number again : ')
-        elif check_digit(val):
-            val = input('Please enter a 3-digit number again : ')
-        elif not val[0].isdigit() or not val[1].isdigit() or not val[2].isdigit():
-            val = input('Please enter a 3-digit number again : ')
+        if len(num_3dig) != 3:
+            num_3dig = input('Please enter a 3-digit number again : ')
+        elif check_digit(num_3dig):
+            num_3dig = input('Please enter a 3-digit number again : ')
+        elif not num_3dig[0].isdigit() or not num_3dig[1].isdigit() or not num_3dig[2].isdigit():
+            num_3dig = input('Please enter a 3-digit number again : ')
         else:
             break
 
-    val = list(map(int, val))
+    num_3dig = list(map(int, num_3dig))
 
     
-    if answer[0] == val[0] and answer[1] == val[1] and answer[2] == val[2]:
+    if answer[0] == num_3dig[0] and answer[1] == num_3dig[1] and answer[2] == num_3dig[2]:
         print("!!---------------------------------!!")
         print("Congratulations!! You won this game!!")
         print("!!---------------------------------!!")
         break
     else:
         if n != 10:
-            print("EAT : " + str(eat_judge(answer, val)))
-            print("BITE : " + str(bite_judge(answer, val)))
+            print("EAT : " + str(eat_judge(answer, num_3dig)))
+            print("BITE : " + str(bite_judge(answer, num_3dig)))
             print("It's wrong.\n")
         else:
             answer = map(str, answer)
-            print("You lose this game. The answer is" + ''.join(answer) + ".")
+            print("You lose this game. The answer is " + ''.join(answer) + ".")
 
     n += 1
